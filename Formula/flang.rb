29f11c6a7f457637b0cbe2f2817de8ef5b8f57e7
Marat Zimnurov <zimtir@mail.ru>
2026-09-09 17:25:44 +0000

    flang 0.7.16: url, sha256, version

--- Formula/flang.rb
+++ Formula/flang.rb
@@ -1,10 +1,10 @@
 class Flang < Formula
   desc "Проверяемый язык: исполняемая спецификация, печатается в восемь языков"
   homepage "https://github.com/digitable-lol/flang"
-  url "https://github.com/digitable-lol/flang/releases/download/v0.7.15/flang-0.7.15-c.tar.gz"
-  sha256 "740571c8d140b1399e73a4331f7805979ed1483ac93da5cd70e898bcaff1dd5b"
+  url "https://github.com/digitable-lol/flang/releases/download/v0.7.16/flang-0.7.16-c.tar.gz"
+  sha256 "5f227c08274879ca981db211566b54bb822a13273568169d48e2e4752df784ee"
   license "BSD-2-Clause"
-  version "0.7.15"
+  version "0.7.16"
 
   depends_on "make" => :build
 
